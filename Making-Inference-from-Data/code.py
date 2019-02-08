1f7b2038f886c150841bee5953535f5efe763ee4
Jasraj Date <jasraj.date@gmail.com>
2019-02-08 18:34:16 +0530

    Code file created.

---
+++ Making-Inference-from-Data/code.py
@@ -0,0 +1,113 @@
+# --------------
+import pandas as pd
+import scipy.stats as stats
+import math
+import numpy as np
+import warnings
+
+warnings.filterwarnings('ignore')
+#Sample_Size
+sample_size=2000
+
+#Z_Critical Score
+z_critical = stats.norm.ppf(q = 0.95)  
+
+
+# path        [File location variable]
+
+#Code starts here
+data = pd.read_csv(path)
+print(len(data))
+
+data_sample = data.sample(2000, random_state=0)
+
+sample_mean = data_sample['installment'].mean()
+
+sample_std = data_sample['installment'].std()
+
+margin_of_error = z_critical*sample_std/math.sqrt(sample_size)
+
+confidence_interval = (sample_mean-margin_of_error, sample_mean+margin_of_error)
+
+true_mean = data['installment'].mean()
+
+print(true_mean)
+
+
+# --------------
+import matplotlib.pyplot as plt
+import numpy as np
+
+#Different sample sizes to take
+sample_size=np.array([20,50,100])
+
+#Code starts here
+
+fig, axes = plt.subplots(3, 1, sharex=True, sharey=True)
+
+for i in range(len(sample_size)):
+    m=list()
+    for j in range(1000):
+        mean=data['installment'].sample(sample_size[i]).mean()
+        m.append(mean)
+    mean_series = pd.Series(m)
+
+
+
+# --------------
+#Importing header files
+
+from statsmodels.stats.weightstats import ztest
+
+#Code starts here
+
+# Removing the last character from the values in column
+data['int.rate'] = data['int.rate'].map(lambda x: str(x)[:-1])
+
+#Dividing the column values by 100
+data['int.rate']=data['int.rate'].astype(float)/100
+
+
+
+#Applying ztest for the hypothesis
+z_statistic, p_value = ztest(x1=data[data['purpose']=='small_business']['int.rate'], value=data['int.rate'].mean(), alternative='larger')
+
+print(('Z-statistic is :{}'.format(z_statistic)))
+print(('P-value is :{}'.format(p_value)))
+
+#Code ends here
+
+
+# --------------
+#Importing header files
+from statsmodels.stats.weightstats import ztest
+
+#Code starts here
+z_statistic, p_value = ztest(data[data['paid.back.loan']=='No']['installment'],data[data['paid.back.loan']=='Yes']['installment'])
+
+# if p_value < 0.05 :
+z_statistic = round(z_statistic, 2)
+
+
+# --------------
+#Importing header files
+from scipy.stats import chi2_contingency
+#Critical value 
+critical_value = stats.chi2.ppf(q = 0.95, # Find the critical value for 95% confidence*
+                      df = 6)   # Df = number of variable categories(in purpose) - 1
+
+#Code starts here
+yes = data[data['paid.back.loan']=='Yes']['purpose'].value_counts()
+
+    
+no = data[data['paid.back.loan']=='No']['purpose'].value_counts()
+
+observed = pd.concat([yes.transpose(),no.transpose()], 1,keys=['Yes','No'])
+
+chi2,p,dof,ex = chi2_contingency(observed)
+
+if chi2 > critical_value:
+    print('reject null')
+
+
+
